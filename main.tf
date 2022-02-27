e70a00cd6d0aeeb377f897a92c30809f5f339d3f
Aaron Lake <aaron.lake@gmail.com>
2022-02-26 21:58:58 -0500

    Update

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "vpc" {
   azs                = var.azs
   public_subnets     = var.public_subnets
   private_subnets    = var.private_subnets
+  database_subnets   = var.database_subnets
   enable_nat_gateway = var.enable_nat_gateway
 
   # Global Tagging Requirements
