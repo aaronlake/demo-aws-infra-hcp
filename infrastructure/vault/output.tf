ed5b01aea3d7da78911e9f5eb3af4ddfb4ceb612
Aaron Lake <aaron.lake@gmail.com>
2022-03-11 14:01:10 -0500

    Add output

--- infrastructure/vault/output.tf
+++ infrastructure/vault/output.tf
@@ -5,3 +5,7 @@ output "vault_private_endpoint_url" {
 output "vault_public_endpoint_url" {
   value = hcp_vault_cluster.this.vault_public_endpoint_url
 }
+
+output "hvn_cidr_block" {
+  value = var.hvn_cidr_block
+}
